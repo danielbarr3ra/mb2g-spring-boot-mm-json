a49bb1ee7c7e1e93d50a3d795858a2b36d3a4933
danielbarr3ra <daniel.er.barrera@gmail.com>
2021-07-04 18:41:15 -0500

    setting up

---
+++ src/main/java/guru/springframework/json/mb2gspringbootmmjson/Mb2gSpringBootMmJsonApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.json.mb2gspringbootmmjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mb2gSpringBootMmJsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mb2gSpringBootMmJsonApplication.class, args);
+    }
+
+}
